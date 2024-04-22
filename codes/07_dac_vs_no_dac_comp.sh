f3027613735f76ac4131a853801ebb11f9fc59ff
Adam Michael Bauer <adammb4@illinois.edu>
2024-04-22 13:22:30 -0500

    corrected error in calibration file

--- codes/07_dac_vs_no_dac_comp.sh
+++ codes/07_dac_vs_no_dac_comp.sh
@@ -8,10 +8,10 @@ python simulation_mains/invBase_cvxpy_main.py ar6_15 1 1
 echo 'Models with uncertainty...'
 echo 'No DAC...'
 
-python simulation_mains/invRec_cvxpy_main.py ar6_15 N1_T40_B6 6 1 1
+python simulation_mains/invRec_cvxpy_main.py ar6_15 N1_T40_B6 1 1
 
 echo 'With DAC...'
-python simulation_mains/invRecBS_cvxpy_main.py ar6bs_15 N1_T40_B6 6 1 1
+python simulation_mains/invRecBS_cvxpy_main.py ar6bs_15 N1_T40_B6 1 1
 
 echo 'Making Figure 7...'
 
