4a4ec96471fef7f7c160a8948405d5d473cb0f22
Adam Michael Bauer <adammb4@illinois.edu>
2024-04-24 14:36:52 -0500

    added truncated methods in tree.py

--- codes/simulation_mains/src/tree.py
+++ codes/simulation_mains/src/tree.py
@@ -10,136 +10,8 @@ import os
 
 import numpy as np
 
-class TreePathIndep():
-    """Tree class
-
-    This class contains the attributes of the information tree used in the N-period optimization model.
-
-    Attributes
-    ----------
-    N_periods: int
-        number of periods
-    
-    nodes_added_per_period: int
-        number of additional nodes each time we learn
-
-    var_mean: float
-        average value of variable distribution
-    
-    var_std: float
-        standard deviation of variable distribution
-
-    full_data: dict
-        parameter values in each period at each node
-
-    full_probs: dict
-        parameter probabilities in each period at each node
-
-    Properties
-    ----------
-    N_nodes_per_period: list
-        number of nodes in each period
-
-    Methods
-    -------
-    initialize_tree_data:
-        generates attriutes full_data and full_probs from variable distribution
-    """
-
-    def __init__(self, N_periods, nodes_added_per_period, var_mean, var_std):
-        self.N_periods = N_periods
-        self.nodes_added_per_period = nodes_added_per_period
-        self.var_mean = var_mean
-        self.var_std = var_std
-
-    @property
-    def N_nodes_per_period(self):
-        N_nodes_per_period = []
-        for i in range(len(self.full_data.keys())):
-            N_nodes_per_period.append(len(self.full_data[i]))
-        return N_nodes_per_period
-
-    def initialize_tree_data(self, check_generated_data=False):
-        """Generate path independent tree with data from a distribution.
-
-        This function creates two dictionaries, both of which store data and probabilities
-        related to an assumed Gaussian distribution of a variable. The final period data
-        is generated from the distribution, and the data at each prior period is found
-        by taking expectations using backward induction.
-
-        Parameters
-        ----------
-        check_generated_data: bool (False)
-            print out generated data tree as a check
-        """
-        
-        # make empty dictionaries to populate
-        self.full_data = {}
-        self.full_probs = {}
-
-        N_periods_loop = self.N_periods - 1 # we only add nodes for periods not including the first
-
-        # number of final states in the tree, dictates how many draws from the distribution we take
-        N_final_states = self.nodes_added_per_period * N_periods_loop + 1
-        
-        # make data for final period
-        end_data = np.random.normal(self.var_mean, self.var_std, size=N_final_states)
-        end_data = sorted(end_data)[::-1]
-        end_probs = [1/len(end_data)] * len(end_data)
-
-        # check generated data and probabilities if you would like
-        if check_generated_data:
-            print("Sorted end data: ", end_data)
-            print("End probs: ", end_probs)
-
-        # loop through periods backwards and populate the full_data and full_probs dictionaries
-        for per in range(N_periods_loop, -1, -1):
-            # if we're in the final period, set the data and probs equal to end period values
-            if per == N_periods_loop:
-                self.full_data[per] = end_data
-                self.full_probs[per] = end_probs
-            
-            # if we're in the first period, we simply use the expectation of the entire distribution
-            elif per == 0:
-                self.full_data[per] = [self.var_mean]
-                self.full_probs[per] = [1.0]
-            
-            # if we're in an intermediate period, use *next period*'s values to compute expectations of what
-            # the value would be at that node
-            else:
-                # total number of nodes in next period
-                next_period_total_states = self.nodes_added_per_period * (per + 1) + 1
-                
-                # make temporary indices to fill
-                tmp_ind_1 = 0
-                tmp_ind_2 = self.nodes_added_per_period
-                
-                # make empty arrays to populate data
-                tmp_data = []
-                tmp_probs = []
-
-                # loop through the upper bound index (tmp_ind_2) until we've exceeded the number of 
-                # states in the next period. that's how we know we're done.
-                while tmp_ind_2 < next_period_total_states:
-                    # values and probabilities at range of next nodes to be used in the expectation
-                    vals = self.full_data[per+1][tmp_ind_1:tmp_ind_2+1]
-                    probs = self.full_probs[per+1][tmp_ind_1:tmp_ind_2+1]
-                    
-                    # take expectation
-                    exp_val = sum([probs[i] * vals[i] for i in range(len(vals))])/ sum(probs)
-                    tmp_data.append(exp_val)
-                    tmp_probs.append(sum(probs))
-                    
-                    # index temporary indices
-                    tmp_ind_1 += 1
-                    tmp_ind_2 += 1
-                
-                # NORMALIZE PROBABILITIES TO ONE
-                tmp_probs_norm = [prob/sum(tmp_probs) for prob in tmp_probs]
-                
-                # populate final dictionaries
-                self.full_data[per] = tmp_data
-                self.full_probs[per] = tmp_probs_norm
+from scipy.optimize import root
+from scipy.integrate import quad 
 
 class TreePathDep():
     """Tree class
@@ -200,23 +72,123 @@ class TreePathDep():
             N_nodes_per_period.append(len(self.full_data[i]))
         return N_nodes_per_period
 
-    def initialize_tree_data(self, check_generated_data=False):
+    def initialize_tree_data(self, check_generated_data=False, trunc_percentile=None):
+        """Initialize tree data.
+
+        Fill in the tree data.
+
+        Parameters
+        ----------
+        check_generated_data: bool (default False)
+            prints big message to report the generated data
+
+        trunc_percentile: int (default None)
+            percentile to truncate distribution at for GHQ_TRUNC method.
+            raises error if not passed
+        """
         if self.method == "MC":
             self._initialize_tree_data_MC(check_generated_data)
 
         elif self.method == "GHQ":
             self._initialize_tree_data_GHQ(check_generated_data)
 
+        elif self.method == "GHQ_TRUNC":
+            if trunc_percentile == None:
+                raise ValueError("No truncation percentile passed. Please add to initialize_tree_data function call if using GHQ_TRUNC method for tree data.")
+            
+            self._initialize_tree_data_GHQ_TRUNC(check_generated_data, trunc_percentile)
+
         else:
             raise ValueError("Invalid setup method. Specify MC -- for Monte Carlo -- or GHQ -- for Gauss-Hermite quadrature.")
 
-    def _initialize_tree_data_GHQ(self, check_generated_data=False):
+    def _initialize_tree_data_MC(self, check_generated_data=False):
         """Generate path independent tree with data from a distribution.
 
         This function creates two dictionaries, both of which store data and probabilities
         related to an assumed Gaussian distribution of a variable. The final period data
         is generated from the distribution, and the data at each prior period is found
         by taking expectations using backward induction.
+
+        Parameters
+        ----------
+        check_generated_data: bool (False)
+            print out generated data tree as a check
+        """
+        
+        # make empty dictionaries to populate
+        self.full_data = {}
+        self.full_probs = {}
+
+        N_periods_loop = self.N_periods - 1 # we only add nodes for periods not including the first
+
+        # number of final states in the tree, dictates how many draws from the distribution we take
+        N_final_states = self.base**N_periods_loop
+        
+        # make data for final period
+        end_data = np.random.normal(self.var_mean, self.var_std, size=N_final_states)
+        end_data = sorted(end_data)[::-1]
+        end_probs = [1/len(end_data)] * len(end_data)
+
+        # check generated data and probabilities if you would like
+        if check_generated_data:
+            print("Sorted end data: ", end_data)
+            print("End probs: ", end_probs)
+
+        # loop through periods backwards and populate the full_data and full_probs dictionaries
+        for per in range(N_periods_loop, -1, -1):
+            # if we're in the final period, set the data and probs equal to end period values
+            if per == N_periods_loop:
+                self.full_data[per] = end_data
+                self.full_probs[per] = end_probs
+            
+            # if we're in the first period, we simply use the expectation of the entire distribution
+            elif per == 0:
+                self.full_data[per] = [self.var_mean]
+                self.full_probs[per] = [1.0]
+            
+            # if we're in an intermediate period, use *next period*'s values to compute expectations of what
+            # the value would be at that node
+            else:
+                # total number of nodes in next period
+                next_period_total_states = self.base**(per + 1)
+                
+                # make temporary indices to fill
+                tmp_ind_1 = 0
+                tmp_ind_2 = self.base
+                
+                # make empty arrays to populate data
+                tmp_data = []
+                tmp_probs = []
+
+                # loop through the upper bound index (tmp_ind_2) until we've exceeded the number of 
+                # states in the next period. that's how we know we're done.
+                while tmp_ind_2 < next_period_total_states+1:
+                    # values and probabilities at range of next nodes to be used in the expectation
+                    vals = self.full_data[per+1][tmp_ind_1:tmp_ind_2]
+                    probs = self.full_probs[per+1][tmp_ind_1:tmp_ind_2]
+                    
+                    # take expectation
+                    exp_val = sum([probs[i] * vals[i] for i in range(len(vals))])/ sum(probs)
+                    tmp_data.append(exp_val)
+                    tmp_probs.append(sum(probs))
+                    
+                    # index temporary indices
+                    tmp_ind_1 += self.base
+                    tmp_ind_2 += self.base
+                
+                # NORMALIZE PROBABILITIES TO ONE
+                tmp_probs_norm = [prob/sum(tmp_probs) for prob in tmp_probs]
+                
+                # populate final dictionaries
+                self.full_data[per] = tmp_data
+                self.full_probs[per] = tmp_probs_norm
+
+    def _initialize_tree_data_GHQ(self, check_generated_data=False):
+        """Generate path independent tree with data from a distribution.
+
+        This function creates two dictionaries, both of which store data and probabilities
+        related to an assumed Gaussian distribution of a variable. This method uses an unbounded
+        Gauss-Hermite quadrature method to approximate the tree data.
 
         Parameters
         ----------
@@ -300,84 +272,78 @@ class TreePathDep():
     def _get_trans_roots(self, roots):
         return self.var_mean + np.sqrt(2) * self.var_std * roots
 
-    def _initialize_tree_data_MC(self, check_generated_data=False):
+    def _initialize_tree_data_GHQ_TRUNC(self, check_generated_data=False, trunc_percentile=None):
         """Generate path independent tree with data from a distribution.
 
         This function creates two dictionaries, both of which store data and probabilities
-        related to an assumed Gaussian distribution of a variable. The final period data
-        is generated from the distribution, and the data at each prior period is found
-        by taking expectations using backward induction.
+        related to an assumed Gaussian distribution of a variable. This method uses an unbounded
+        Gauss-Hermite quadrature method to approximate the tree data.
+
+        We use the Cai and Judd (2015) methodology, see:
+        https://doi.org/10.1007/s00186-015-0495-z
 
         Parameters
         ----------
         check_generated_data: bool (False)
             print out generated data tree as a check
+        
+        trunc_percentile: int
+            percentile to cutoff distribution at
         """
         
         # make empty dictionaries to populate
         self.full_data = {}
         self.full_probs = {}
 
-        N_periods_loop = self.N_periods - 1 # we only add nodes for periods not including the first
+        # find roots of Hermite polynomials to evaluate distribution at
+        roots = self._gen_ghq_roots()
 
-        # number of final states in the tree, dictates how many draws from the distribution we take
-        N_final_states = self.base**N_periods_loop
+        # compute weights via the usual formula
+        self.weights = self._gen_ghq_weights(roots)
+
+        # find transformation to cutoff distribution
+        # use sample average approximation to find the percentile value 
+        # (i.e., the raw number, rather than the percentile number)
+        perc_cutoff_samp = [np.percentile(np.random.normal(self.var_mean, self.var_std, int(1e5)), trunc_percentile) for i in range(1000)]
         
-        # make data for final period
-        end_data = np.random.normal(self.var_mean, self.var_std, size=N_final_states)
-        end_data = sorted(end_data)[::-1]
-        end_probs = [1/len(end_data)] * len(end_data)
+        # take avg
+        perc_cutoff = np.mean(perc_cutoff_samp)
+
+        # transformation factor for cutoff distribution
+        self.trans_factor = (self.var_mean - perc_cutoff) * self.var_std**(-1)
+
+        # find 'k' factor for the transformation
+        self.k = self._get_scale(bound=self.trans_factor)
+
+        # transform roots following the Cai and Judd transformation
+        self.trans_roots_cutoff = self._get_cutoff_roots(roots, self.trans_factor, self.k)
 
         # check generated data and probabilities if you would like
         if check_generated_data:
-            print("Sorted end data: ", end_data)
-            print("End probs: ", end_probs)
+            print("Sorted data: ", self.trans_roots_cutoff)
+            print("Weights: ", self.weights)
+        
+        self.full_data[0] = np.array([self.var_mean])
+        self.full_probs[0] = np.array([1.0])
 
-        # loop through periods backwards and populate the full_data and full_probs dictionaries
-        for per in range(N_periods_loop, -1, -1):
-            # if we're in the final period, set the data and probs equal to end period values
-            if per == N_periods_loop:
-                self.full_data[per] = end_data
-                self.full_probs[per] = end_probs
-            
-            # if we're in the first period, we simply use the expectation of the entire distribution
-            elif per == 0:
-                self.full_data[per] = [self.var_mean]
-                self.full_probs[per] = [1.0]
-            
-            # if we're in an intermediate period, use *next period*'s values to compute expectations of what
-            # the value would be at that node
-            else:
-                # total number of nodes in next period
-                next_period_total_states = self.base**(per + 1)
-                
-                # make temporary indices to fill
-                tmp_ind_1 = 0
-                tmp_ind_2 = self.base
-                
-                # make empty arrays to populate data
-                tmp_data = []
-                tmp_probs = []
+        self.full_data[1] = self.trans_roots_cutoff
+        self.full_probs[1] = self.weights
+    
+    def _get_scale(self, bound):
+        sol = root(self._diff, x0=[0.5], args=[bound])
+        return sol.x[0]
 
-                # loop through the upper bound index (tmp_ind_2) until we've exceeded the number of 
-                # states in the next period. that's how we know we're done.
-                while tmp_ind_2 < next_period_total_states+1:
-                    # values and probabilities at range of next nodes to be used in the expectation
-                    vals = self.full_data[per+1][tmp_ind_1:tmp_ind_2]
-                    probs = self.full_probs[per+1][tmp_ind_1:tmp_ind_2]
-                    
-                    # take expectation
-                    exp_val = sum([probs[i] * vals[i] for i in range(len(vals))])/ sum(probs)
-                    tmp_data.append(exp_val)
-                    tmp_probs.append(sum(probs))
-                    
-                    # index temporary indices
-                    tmp_ind_1 += self.base
-                    tmp_ind_2 += self.base
-                
-                # NORMALIZE PROBABILITIES TO ONE
-                tmp_probs_norm = [prob/sum(tmp_probs) for prob in tmp_probs]
-                
-                # populate final dictionaries
-                self.full_data[per] = tmp_data
-                self.full_probs[per] = tmp_probs_norm
+    def _diff(self, scale, args):
+        bound, = args
+        piece = bound**2 * quad(self._integrand, -30, 30, args=[scale])[0]
+        return piece - 1
+
+    def _integrand(self, x, args):
+        scale, = args
+        y = 2 * (1 + np.exp(-scale * x))**(-1) - 1
+        z = y * y * np.exp(-1 * x * x * 0.5) * np.sqrt(2 * np.pi)**(-1)
+        return z
+
+    def _get_cutoff_roots(self, x, scaling_factor, k):
+        x = np.array(x)
+        return self.var_mean + scaling_factor * (1 - np.exp(-k * x)) * (1 + np.exp(-k * x))**(-1) * self.var_std
